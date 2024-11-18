46f5a6eb544df8c3c41af78ce9cfce0f6b26ca51
Tharusha Perera <tharushaperera6199@gmail.com>
2024-11-18 20:51:58 +0530

    update the buttons

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -2,21 +2,135 @@ import 'package:delivery_x/widget/widget_support.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
-  const Home({Key? key}) : super(key: key);
+  const Home({super.key});
 
   @override
   State<Home> createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
+  bool ice_cream = false, pizza = false, salad = false, burgur = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        margin: EdgeInsets.only(top: 50.0, left: 20.0, right: 10.0),
+        margin: const EdgeInsets.only(top: 50.0, left: 20.0, right: 10.0),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text("Hello Tharusha", style: AppWidget.boldTextFeildStyle())
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text("Hello Tharusha", style: AppWidget.boldTextFeildStyle()),
+                Container(
+                  padding: const EdgeInsets.all(3),
+                  decoration: BoxDecoration(
+                      color: Colors.black,
+                      borderRadius: BorderRadius.circular(8)),
+                  child: const Icon(
+                    Icons.shopping_cart_outlined,
+                    color: Colors.white,
+                  ),
+                )
+              ],
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Text(
+              "Delicious Food",
+              style: AppWidget.headlineTextFeildStyle(),
+            ),
+            Text(
+              'Discover and get Great Food',
+              style: AppWidget.lightTextFeildStyle(),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                GestureDetector(
+                  onTap: () => {
+                    pizza = true,
+                    salad = false,
+                    burgur = false,
+                    ice_cream = false,
+                    setState(() {})
+                  },
+                  child: Material(
+                    elevation: 5.0,
+                    borderRadius: BorderRadius.circular(10),
+                    child: Container(
+                      decoration: BoxDecoration(
+                          color: pizza ? Colors.black : Colors.white,
+                          borderRadius: BorderRadius.circular(10)),
+                      padding: EdgeInsets.all(10),
+                      child: Image.asset(
+                        "images/pizza.png",
+                        color: pizza ? Colors.white : Colors.black,
+                        height: 50.0,
+                        width: 50.0,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                ),
+                GestureDetector(
+                  onTap: () => {
+                    salad = true,
+                    pizza = false,
+                    ice_cream = false,
+                    burgur = false,
+                    setState(() {})
+                  },
+                  child: Material(
+                    elevation: 5.0,
+                    borderRadius: BorderRadius.circular(10),
+                    child: Container(
+                      decoration: BoxDecoration(
+                          color: salad ? Colors.black : Colors.white,
+                          borderRadius: BorderRadius.circular(10)),
+                      padding: EdgeInsets.all(10),
+                      child: Image.asset(
+                        "images/salad.png",
+                        color: salad ? Colors.white : Colors.black,
+                        height: 50.0,
+                        width: 50.0,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                ),
+                Material(
+                  elevation: 5.0,
+                  borderRadius: BorderRadius.circular(10),
+                  child: Container(
+                    padding: EdgeInsets.all(10),
+                    child: Image.asset(
+                      "images/burger.png",
+                      height: 50.0,
+                      width: 50.0,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+                Material(
+                  elevation: 5.0,
+                  borderRadius: BorderRadius.circular(10),
+                  child: Container(
+                    padding: EdgeInsets.all(10),
+                    child: Image.asset(
+                      "images/ice-cream.png",
+                      height: 50.0,
+                      width: 50.0,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                )
+              ],
+            )
           ],
         ),
       ),
--- lib/widget/widget_support.dart
+++ lib/widget/widget_support.dart
@@ -4,10 +4,28 @@ import 'package:flutter/material.dart';
 
 class AppWidget {
   static TextStyle boldTextFeildStyle() {
-    return TextStyle(
+    return const TextStyle(
       color: Colors.black,
       fontWeight: FontWeight.bold,
       fontSize: 20,
+      fontFamily: 'Poppins',
+    );
+  }
+
+  static TextStyle headlineTextFeildStyle() {
+    return const TextStyle(
+      color: Colors.black,
+      fontWeight: FontWeight.bold,
+      fontSize: 24,
+      fontFamily: 'Poppins',
+    );
+  }
+
+  static TextStyle lightTextFeildStyle() {
+    return const TextStyle(
+      color: Colors.black38,
+      fontWeight: FontWeight.w500,
+      fontSize: 15,
       fontFamily: 'Poppins',
     );
   }
